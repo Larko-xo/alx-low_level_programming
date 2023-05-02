1d1ef2428ad4ea6ce62225a27b812382a30da09a
Larko-xo <darkolaura2@gmail.com>
2023-05-02 15:26:28 -0700

    updates

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop – It finds the loop in a linked list;
+ * @head: linked list to search for;
+ *
+ * Return: address of the node where the loop starts, or NULL
+ */
+
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *hier = head;
+	listint_t *today = head;
+
+	if (!head)
+		return (NULL);
+
+	while (hier && today && today->next)
+	{
+		today = today->next->next;
+		hier = hier->next;
+		if (today == hier)
+		{
+			hier = head;
+			while (hier != today)
+			{
+				hier = hier->next;
+				today = today->next;
+			}
+			return (today);
+		}
+	}
+
+	return (NULL);
+}
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,13 +1,15 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - insert a new node at a given position in a linked list;
+ * insert_nodeint_at_index - to insert a new node at a
+ * given position in a linked list;
  * @head: pointer to the 1st node in the list;
  * @idx: index where the NEW node is added
  * @n: data to insert in the new node
  *
  * Return: pointer to the new node, or NULL
  */
+
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i;
