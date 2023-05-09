dd2d53d465839aac1e3335c1c9b75bf9198405cf
Larko-xo <darkolaura2@gmail.com>
2023-05-08 17:24:12 -0700

    C-File I/O

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * create_file - Creates a file.
+ * @thefilename: A pointer to the name of the file to create.
+ * @txt_cntnt: A pointer to a string to write to the file.
+ *
+ * Return: If the function fails - -1.
+ *         Otherwise - 1.
+ */
+int create_file(const char *thefilename, char *txt_cntnt)
+{
+	int ab, r, tem = 0;
+
+	if (thefilename == NULL)
+		return (-1);
+
+	if (txt_cntnt != NULL)
+	{
+		for (tem = 0; txt_cntnt[tem];)
+			tem++;
+	}
+
+	ab = open(thefilename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	r = write(ab, txt_cntnt, pem);
+
+	if (ab == -1 || r == -1)
+		return (-1);
+
+	close(ab);
+
+	return (1);
+}
